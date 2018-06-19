20946ce1034d210a492289852b622f24838b28a6
George Thomas <tgeorgetom@gmail.com>
2018-06-19 23:47:55 +0530

    Roadmap Slider

--- js/custom.js
+++ js/custom.js
@@ -38,3 +38,37 @@ $('.section-5-slides').slick({
   ]
 });
 	
+
+$('.roadmapTimeline-contents').slick({
+  centerMode: true,
+  centerPadding: '100px',
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        centerMode: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerPadding: '0px'
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+  
